e37c01e554beb0d367da9f410c43976bcc1d3bc7
Shahab Alam <shahabalam2512@gmail.com>
2026-09-20 17:03:24 +0530

    Initial multi-agent research system

---
+++ tools.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain.tools import tool
+from tavily import TavilyClient
+from langchain.agents import create_agent
+from langchain.agents.middleware import wrap_tool_call
+from langchain.messages import ToolMessage
+from rich import print
+from bs4 import BeautifulSoup
+
+import os
+import requests
+
+load_dotenv()
+
+
+
+#  TOOL - 1
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+@tool
+def web_search(query : str) ->str:
+    #doc string 
+    """Search the web for recent and reliable information on a topic. Return Titles, URLs and snippets """
+    results = tavily.search(query=query, max_results=5)
+
+
+    out = []
+
+    for r in results['results']:
+       out.append(
+        f"Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'] [:300]}\n"
+    )
+
+    return "\n-----\n".join(out)
+
+
+
+
+#  TOOL - 2
+@tool
+def scrape_url(url: str) -> str:
+    # DOC STRING 
+    """Scrape and return clean text content from a given URL for deeper reading."""
+    try:
+        resp = requests.get(url, timeout=8, headers={"User-Agent": "Mozilla/5.0"})
+        soup = BeautifulSoup(resp.text, "html.parser")
+
+        for tag in soup(["script", "style", "nav", "footer"]):
+            tag.decompose()
+
+        return soup.get_text(separator=" ", strip=True)[:3000]
+
+    except Exception as e:
+        return f"Could not scrape URL: {str(e)}"
+
+# print(scrape_url.invoke("https://www.cricbuzz.com/cricket-news/140231/athapaththu-fires-sri-lanka-into-asian-games-final"))
